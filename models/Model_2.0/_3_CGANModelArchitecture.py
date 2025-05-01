b52861e101e1db9bb52001196fc7ad6fe762a666
MazharRehan <mazharrehan80@gmail.com>
2025-05-02 00:33:40 +0500

    Model 2.0 training

--- models/Model_2.0/_3_CGANModelArchitecture.py
+++ models/Model_2.0/_3_CGANModelArchitecture.py
@@ -1,146 +1,75 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras import layers, models
 
-
-def build_generator(latent_dim, condition_dim, output_shape=(256, 256, 3)):
-    """
-    Build generator model for CGAN.
-
-    Args:
-        latent_dim: Dimension of random noise input
-        condition_dim: Dimension of condition vector (room counts)
-        output_shape: Shape of output image
-
-    Returns:
-        Keras Model
-    """
-    # Random noise input
+def build_generator(latent_dim, condition_dim, output_shape=(560, 1014, 3)):
     noise_input = layers.Input(shape=(latent_dim,))
-
-    # Condition input (room counts)
     condition_input = layers.Input(shape=(condition_dim,))
-
-    # Concatenate noise and condition
-    combined_input = layers.Concatenate()([noise_input, condition_input])
-
-    # First dense layer with enough units for 8x8 feature maps with 256 channels
-    x = layers.Dense(8 * 8 * 256)(combined_input)
+    x = layers.Concatenate()([noise_input, condition_input])
+    x = layers.Dense(8 * 8 * 256)(x)
     x = layers.LeakyReLU(0.2)(x)
     x = layers.Reshape((8, 8, 256))(x)
-
-    # Transposed convolution blocks (upsampling)
-    # 8x8 -> 16x16
-    x = layers.Conv2DTranspose(128, 4, strides=2, padding='same')(x)
+    x = layers.Conv2DTranspose(128, 4, strides=2, padding='same')(x)  # 16x16
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
-    # 16x16 -> 32x32
-    x = layers.Conv2DTranspose(64, 4, strides=2, padding='same')(x)
+    x = layers.Conv2DTranspose(64, 4, strides=2, padding='same')(x)   # 32x32
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
-    # 32x32 -> 64x64
-    x = layers.Conv2DTranspose(32, 4, strides=2, padding='same')(x)
+    x = layers.Conv2DTranspose(32, 4, strides=2, padding='same')(x)   # 64x64
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
-    # 64x64 -> 128x128
-    x = layers.Conv2DTranspose(32, 4, strides=2, padding='same')(x)
+    # Upsample to target shape
+    x = layers.Conv2DTranspose(32, 4, strides=(target_shape_div(output_shape, 64)), padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
-    # 128x128 -> 256x256
-    x = layers.Conv2DTranspose(16, 4, strides=2, padding='same')(x)
+    x = layers.Conv2DTranspose(16, 4, strides=1, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
-    # Final output layer with tanh activation (generates images in [-1, 1])
     output = layers.Conv2D(3, 3, padding='same', activation='tanh')(x)
-
-    # Create and return model
     model = models.Model([noise_input, condition_input], output)
     return model
 
+def target_shape_div(output_shape, source_dim):
+    # Returns (stride_h, stride_w) to reach output_shape from source_dim
+    stride_h = output_shape[1] // source_dim
+    stride_w = output_shape[0] // source_dim
+    return (stride_h, stride_w)
 
-def build_discriminator(input_shape=(256, 256, 3), condition_dim=None):
-    """
-    Build discriminator model for CGAN.
-
-    Args:
-        input_shape: Shape of input image
-        condition_dim: Dimension of condition vector
-
-    Returns:
-        Keras Model
-    """
-    # Image input
+def build_discriminator(input_shape=(560, 1014, 3), condition_dim=None):
     image_input = layers.Input(shape=input_shape)
-
-    # Condition input
     condition_input = layers.Input(shape=(condition_dim,))
-
-    # Process condition through dense layers
-    condition_x = layers.Dense(input_shape[0] * input_shape[1])(condition_input)
-    condition_x = layers.Reshape((input_shape[0], input_shape[1], 1))(condition_x)
-
-    # Concatenate image with condition along channel dimension
-    x = layers.Concatenate()([image_input, condition_x])
-
-    # Convolutional layers
+    cond = layers.Dense(8 * 8 * 1)(condition_input)
+    cond = layers.LeakyReLU(0.2)(cond)
+    cond = layers.Reshape((8, 8, 1))(cond)
+    cond = layers.Lambda(lambda x: tf.image.resize(x, (input_shape[0], input_shape[1]), method='nearest'))(cond)
+    x = layers.Concatenate()([image_input, cond])
     x = layers.Conv2D(32, 4, strides=2, padding='same')(x)
     x = layers.LeakyReLU(0.2)(x)
-
+    x = layers.Dropout(0.3)(x)
     x = layers.Conv2D(64, 4, strides=2, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
+    x = layers.Dropout(0.3)(x)
     x = layers.Conv2D(128, 4, strides=2, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
+    x = layers.Dropout(0.3)(x)
     x = layers.Conv2D(256, 4, strides=2, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
+    x = layers.Dropout(0.3)(x)
     x = layers.Conv2D(512, 4, strides=2, padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU(0.2)(x)
-
-    # Flatten and output single value
     x = layers.Flatten()(x)
     output = layers.Dense(1, activation='sigmoid')(x)
-
     model = models.Model([image_input, condition_input], output)
     return model
 
-
-# Build the CGAN
 def build_cgan(latent_dim, condition_dim, generator, discriminator):
-    """
-    Build the complete CGAN model.
-
-    Args:
-        latent_dim: Dimension of random noise input
-        condition_dim: Dimension of condition vector
-        generator: Generator model
-        discriminator: Discriminator model
-
-    Returns:
-        Keras Model
-    """
-    # For the combined model, we only train the generator
     discriminator.trainable = False
-
-    # Random noise and condition inputs
     noise_input = layers.Input(shape=(latent_dim,))
     condition_input = layers.Input(shape=(condition_dim,))
-
-    # Generate an image using the generator
     generated_image = generator([noise_input, condition_input])
-
-    # Classify the generated image with the discriminator
     validity = discriminator([generated_image, condition_input])
-
-    # Build and compile the combined model
     model = models.Model([noise_input, condition_input], validity)
     return model
--- models/Model_2.0/_5_CompleteMainScript_for_Training.py
+++ models/Model_2.0/_5_CompleteMainScript_for_Training.py
@@ -2,66 +2,51 @@ import matplotlib.pyplot as plt
 from _2_DataPreprocessing_for_ModelTraining import preprocess_data
 from _3_CGANModelArchitecture import build_generator, build_discriminator, build_cgan
 from _4_ModelTrainingScript import train_cgan, save_generated_images
-# from keras import optimizers
 from tensorflow.keras.optimizers import Adam
-opt = Adam(0.0001, 0.5, clipvalue=1.0)
+import numpy as np
 
 if __name__ == "__main__":
-    # Parameters
     latent_dim = 100
-    batch_size = 8 # 32
+    batch_size = 8
     epochs = 30
     save_interval = 10
-    image_size = (256, 256)
+    image_size = (560, 1014)  # width, height based on avg aspect ratio
 
-    # Load and preprocess data
-    X_train, X_val, metadata_train, metadata_val = preprocess_data(
+    X_train, X_val, metadata_train, metadata_val, area_per_pixel_train, area_per_pixel_val = preprocess_data(
         "./dataset",
         "floor_plan_metadata_extended.csv",
-        target_size=image_size
+        target_size=image_size,
+        use_areas=False  # Set to True to add room areas to conditioning vector
     )
 
     condition_dim = metadata_train.shape[1]
-
     print(f"Data loaded: {X_train.shape[0]} training samples, {X_val.shape[0]} validation samples")
     print(f"Condition dimension: {condition_dim}")
 
-    # Build models
     generator = build_generator(latent_dim, condition_dim, output_shape=(*image_size, 3))
     discriminator = build_discriminator(input_shape=(*image_size, 3), condition_dim=condition_dim)
 
-    # Compile discriminator
     discriminator.compile(
         loss='binary_crossentropy',
-        optimizer=Adam(0.0001, 0.5, clipvalue=1.0),
-        # optimizer=optimizers.Adam(0.0001, 0.5),
+        optimizer=Adam(1e-4, 0.5, clipvalue=1.0),
         metrics=['accuracy']
     )
-
-    # Build combined CGAN model
     cgan = build_cgan(latent_dim, condition_dim, generator, discriminator)
-
-    # Compile combined model
     cgan.compile(
         loss='binary_crossentropy',
-        optimizer=Adam(0.0001, 0.5, clipvalue=1.0),
-        # optimizer=optimizers.Adam(0.0001, 0.5)
+        optimizer=Adam(1e-4, 0.5, clipvalue=1.0)
     )
 
-    # Print model summaries
     generator.summary()
     discriminator.summary()
 
-    # Train the model
     history = train_cgan(generator, discriminator, cgan,
                          X_train, metadata_train,
                          latent_dim, batch_size, epochs, save_interval)
 
-    # Save final models
     generator.save('models/generator_final.h5')
     discriminator.save('models/discriminator_final.h5')
 
-    # Plot training history
     plt.figure(figsize=(10, 5))
     plt.plot(history['d_losses'], label='Discriminator Loss')
     plt.plot(history['g_losses'], label='Generator Loss')
